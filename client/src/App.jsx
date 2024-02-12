b9b3be0532cf48405ef5d993cd4da669b75be2db
HuGymsForMe <diaz36988@gmail.com>
2024-02-12 14:55:45 +0100

    Deployando...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,12 @@ import LoadingPage from "./pages/LoadingPage";
 
 // ******* PLANTILLAS QUE COMPONEN LA APLICACIÓN ******* //
 const HomePage = lazy(() => import("./pages/HomePage"));
-const TestsPage = lazy(() => import("./pages/TestsPage"));
+// const TestsPage = lazy(() => import("./pages/TestsPage"));
 //const NewsPage = lazy(() => import("./pages/NewsPage"));
 const ContactPage = lazy(() => import("./pages/ContactPage"));
 // const TestPage = lazy(() => import("./pages/TestPage"));
-const LoginPage = lazy(() => import("./pages/LoginPage"));
-const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+// const LoginPage = lazy(() => import("./pages/LoginPage"));
+// const RegisterPage = lazy(() => import("./pages/RegisterPage"));
 // const ProfilePage = lazy(() => import("./pages/ProfilePage"));
 const WorkPage = lazy(() => import("./pages/WorkPage"));
 
