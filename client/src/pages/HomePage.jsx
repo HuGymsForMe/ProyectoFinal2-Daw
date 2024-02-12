040c183e1d6deffba53fd52b2c08cba1050f6bed
HuGymsForMe <diaz36988@gmail.com>
2024-02-12 15:38:36 +0100

    Deployando...

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -50,14 +50,14 @@ function HomePage() {
                     <img src={carLicense} alt="Coche Autoescuela" className="rounded-lg xl:w-[33%] xl:h-[17%] sm:w-[66%] sm:h-[33%] w-[90%] h-[45%]" />
                 </section>
                 <section className="flex xl:flex-row flex-col w-[75%] items-center gap-6">
-                    <div className="flex flex-col items-center px-4 py-8 gap-y-4 rounded-xl bg-white w-[50%] lg:w-[100%]">
+                    <div className="flex flex-col items-center px-4 py-8 gap-y-4 rounded-xl bg-white lg:w-[50%] w-[95%]">
                         <h3 className={`text-[#C21D30] text-bold text-4xl text-center`}>Realizar Test</h3>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`w-14 h-14 fill-[#C21D30]`}>
                             <path fillRule="evenodd" d="M2.25 5.25a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3V15a3 3 0 0 1-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 0 1-.53 1.28h-9a.75.75 0 0 1-.53-1.28l.621-.622a2.25 2.25 0 0 0 .659-1.59V18h-3a3 3 0 0 1-3-3V5.25Zm1.5 0v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5Z" clipRule="evenodd" />
                         </svg>
                         <p className={`text-[#C21D30] text-center`}>¡ Podrás realizar muchos test ! Además de ver en tu perfil de test, tus mejores resultados en cuanto a tiempo y fallos. Nuestro objetivo es que salgas de AutoescuelaFast con todos los test en verde y con tu exámen teórico aprobado.</p>
                     </div>
-                    <div className="flex flex-col items-center px-4 py-8 gap-y-4 rounded-xl bg-white w-[50%] lg:w-[100%]">
+                    <div className="flex flex-col items-center px-4 py-8 gap-y-4 rounded-xl bg-white lg:w-[50%] w-[95%]">
                         <h3 className={`text-[#C21D30] text-bold text-4xl text-center`}>Ver mis estadísticas</h3>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`w-14 h-14 fill-[#C21D30]`}>
                             <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75ZM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 0 1-1.875-1.875V8.625ZM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 0 1 3 19.875v-6.75Z" />
