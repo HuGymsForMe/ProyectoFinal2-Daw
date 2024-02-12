e25d14acc55947ac4b50706039061528d885b11f
HuGymsForMe <diaz36988@gmail.com>
2024-02-12 13:51:39 +0100

    Deployando...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,14 +9,14 @@ import LoadingPage from "./pages/LoadingPage";
 
 // ******* PLANTILLAS QUE COMPONEN LA APLICACIÓN ******* //
 const HomePage = lazy(() => import("./pages/HomePage"));
-const TestsPage = lazy(() => import("./pages/TestsPage"));
-//const NewsPage = lazy(() => import("./pages/NewsPage"));
-const ContactPage = lazy(() => import("./pages/ContactPage"));
-const TestPage = lazy(() => import("./pages/TestPage"));
-const LoginPage = lazy(() => import("./pages/LoginPage"));
-const RegisterPage = lazy(() => import("./pages/RegisterPage"));
-const ProfilePage = lazy(() => import("./pages/ProfilePage"));
-const WorkPage = lazy(() => import("./pages/WorkPage"));
+// const TestsPage = lazy(() => import("./pages/TestsPage"));
+// //const NewsPage = lazy(() => import("./pages/NewsPage"));
+// const ContactPage = lazy(() => import("./pages/ContactPage"));
+// const TestPage = lazy(() => import("./pages/TestPage"));
+// const LoginPage = lazy(() => import("./pages/LoginPage"));
+// const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+// const ProfilePage = lazy(() => import("./pages/ProfilePage"));
+// const WorkPage = lazy(() => import("./pages/WorkPage"));
 
 function App() {
 
@@ -26,16 +26,15 @@ return (
         <Suspense fallback={<LoadingPage/>}>
           <Routes>
             <Route path="/" element={<HomePage />}></Route>
-            {/*<Route path="/news" element={<NewsPage />}></Route>*/}
-            <Route path="/contact" element={<ContactPage />}></Route>
+            {/* <Route path="/contact" element={<ContactPage />}></Route>
             <Route path="/login" element={<LoginPage />}></Route>
             <Route path="/register" element={<RegisterPage />}></Route>
             <Route path="/work" element={<WorkPage />}></Route>
-            {/*<Route element={<ProtectedRoute />}>*/}
+            <Route element={<ProtectedRoute />}>
               <Route path="/test/:idTest" element={<TestPage />}></Route>
               <Route path="/profile/:idUser" element={<ProfilePage />}></Route>
               <Route path="/tests/:idUser" element={<TestsPage />}></Route>
-            {/*</Route>*/}
+            </Route> */}
           </Routes> 
         </Suspense>
     </BrowserRouter>
