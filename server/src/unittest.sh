dfb84212c9ff2bc73cb479f0e561c4ba0eea1d3e
HuGymsForMe <hdiazcasado@gmail.com>
2024-04-25 13:12:15 +0200

    Archivos BASH

--- server/src/unittest.sh
+++ server/src/unittest.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+#Este fichero se ejecutará de forma diaria.
+
+$directory="/....../server"
+
+#Ejecutamos el comando que checkea nuestros test unitarios y guardamos la salida en una variable
+output=$(cd "$directory" | npm test)
+
+#Recogemos la fecha actual
+DAY_NUM=$(date +%d)
+MONTH_NUM=$(date +%m)
+YEAR_NUM=$(date +%y)
+
+#Pasamos la salida del comando anterior a un fichero de texto que generaremos diariamente
+echo "$output" > log/tracking-"[$DAY_NUM-$MONTH_NUM-$YEAR_NUM]".log
+
+#Autocommit del nuevo fichero en github
+# git add log
+# git commit -m "Actualización de los test unitarios"
+# git push -u origin master
+
+#Ejecutamos el fichero de tracking.sh para borrar los ficheros antiguos (no olvidar de darle permisos)
+./tracking.sh
